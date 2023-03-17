06fb13e7c36e6800a222c1b48de7b03f30086f9e
senroen <2033567015@qq.com>
2023-03-17 08:29:12 +0800

    3.17

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -2,19 +2,35 @@
     <div id ="login">
         <div class="from-wrap">
             <ul class="menu-tab">
-                <li class="current">登录</li>
-                <li>注册</li>
-                <el-form ref="from" >
-                    <el-form-item label="username">
-                        <label class="from-label">用户名</label>
-                        <el-input />
+                <!-- :class == v-bind-->
+                <!-- @click="$event => current_menu=item.type"-->
+                <li @click="_$event => toggleMenu(item.type)" :class="{'current':current_menu===item.type}" v-for="item in tab_menu" :key="item.type">{{ item.label }}</li>
+                <el-form ref="from" :model="form" :rules="form_rules">
+                    <el-form-item  prop="username">
+                        <label class="from-label">邮箱</label>
+                        <el-input v-model.trim="form.username"/>
                     </el-form-item>
-                    <el-form-item label="password">
+                    <el-form-item  prop="password">
                         <label class="from-label">密码</label>
-                        <el-input type="passord"/>
+                        <el-input type="password" v-model.trim="form.password"/>
+                    </el-form-item>
+                    <el-form-item v-if="current_menu==='register'" prop="passwords">
+                        <label class="from-label">确认密码</label>
+                        <el-input type="password" v-model.trim="form.passwords"/>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary" @click=" ">立即创建</el-button>
+                        <label class="from-label">验证码</label>
+                        <el-row :gutter="21">
+                            <el-col :span="14">
+                                <el-input v-model="code"></el-input>
+                            </el-col>
+                            <el-col :span="10">
+                                <el-button type="success" class="el-button-block">获得验证码</el-button>
+                            </el-col>
+                        </el-row>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="danger" class="el-button-block" disabled>登录</el-button>
                     </el-form-item>
                 </el-form>
             </ul>   
@@ -42,7 +58,7 @@
             font-size: 14px;
             border-radius: 5px;
             cursor: pointer;
-            &.current{background-color: rgba(0,0,0,-1);}
+            &.current{background-color: rgba(0,0,0,.1);}
         }
     }
     .from-label{
@@ -50,4 +66,86 @@
         color:#fff;
         font-size: 14px;
     }
+    el-input{
+        display: block;
+        width: 100%;
+    }
 </style>
+<script>
+import { reactive,toRefs } from 'vue';
+export default{
+    setup(_porops,{root}){
+        const data = reactive({
+            form:{
+                username:"",
+                password:"",
+                passwords:"",
+                code:"",
+            },
+            tab_menu:
+            [
+                {type:"login",label:"登录"},
+                {type:"register",label:"注册"}
+            ],
+            // form_rules:{
+            //     username:[
+            //         {required:true,message:'请输入用户名',tigger:'change'},
+            //         {min:3,max:5,message:'长度在3和5之间',tigger:'change'}
+            //     ],
+            //     mima:[
+            //         {required:true,message:'请输入密码',tigger:'change'},
+            //         {min:8,max:16,message:'长度在8和16之间',tigger:'change'}
+            //     ]
+            // },
+            current_menu:"login"
+        })
+        const validata_username_rules =(_rule,value,callback)=>{
+            let reg_email =/^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+            if(value===""){
+                callback(new Error("请输入邮箱"))
+            }else if(!reg_email.test(value)){
+                callback(new Error("请输入正确的邮箱格式"))
+            }else{
+                callback()
+            }
+        }
+        const validata_password_rules =(_rule,value,callback)=>{
+            let reg_password =/^(?!D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+            if(value===""){
+                callback(new Error("请输入密码"))
+            }else if(!reg_password.test(value)){
+                callback(new Error("请输入6-20位密码,包含数字和、字母"))
+            }else{
+                callback()
+            }
+        }
+        const validata_passwords_rules =(rule,value,callback)=>{
+            let reg_password =/^(?!D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+            const passwordValues = data.form.password;
+            if(value===""){
+                callback(new Error("请输入密码"))
+            }else if(!reg_password.test(value)){
+                callback(new Error("请输入6-20位密码,包含数字和、字母"))
+            }
+            else if( passwordValues &&passwordValues !== value){
+                callback(new Error("俩次密码不一致"))
+            }else{
+                callback()
+            }
+        }
+        
+        const form_rules = reactive({
+            username:[{validator:validata_username_rules,trigger:'change'}],
+            password:[{validator:validata_password_rules,trigger:'change'}],
+            passwords:[{validator:validata_passwords_rules,trigger:'change'}]
+        })
+        const toggleMenu = ((type)=>{
+            data.current_menu =type
+        })
+        return{
+            toggleMenu,...toRefs(data),form_rules
+        }
+    }
+
+}
+</script>
